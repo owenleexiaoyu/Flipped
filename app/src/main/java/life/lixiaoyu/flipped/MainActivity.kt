343ae043d20525ee0e7002f73b3a1f3d3715ed42
OwenLee <2924354040@qq.com>
2022-07-05 18:39:06 +0800

    init project

---
+++ app/src/main/java/life/lixiaoyu/flipped/MainActivity.kt
@@ -0,0 +1,11 @@
+package life.lixiaoyu.flipped
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
